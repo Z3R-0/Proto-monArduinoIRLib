0ab9c61a7b5aca538546c8218a9a2a1ac00c842e
TimM <timmeermans@live.nl>
2017-11-26 00:48:36 +0100

    Added comments in ProtomonTransmit

--- Protomon/ProtomonTransmit.cpp
+++ Protomon/ProtomonTransmit.cpp
@@ -1,7 +1,8 @@
-// 
-// 
-// 
-
+/*
+	Name: Tim Meermans
+	Date: 24 November 2017
+	Description: This class is for the transmit function. It includes all required functions for transmit() to work properly.
+*/
 #include "ProtomonTransmit.h"
 extern "C" {
 	#include <string.h>
@@ -22,5 +23,8 @@ void ProtomonTransmit::setFreq(uint8_t freq){
 void ProtomonTransmit::cypherBits(char downID[4]){
 	strcpy(data, "1");
 	strcat(data, downID);
-	//invert the downmon ID
+	//For loop: invert the downmonID
+	for(;;){
+		//make un-infinite
+	}
 }
