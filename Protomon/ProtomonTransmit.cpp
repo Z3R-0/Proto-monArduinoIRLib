194889b17ac0dcb448825a22c21fbd2cd228aff4
TimM <timmeermans@live.nl>
2017-11-26 18:57:16 +0100

    Finished up cypherData(). Now ready for first test

--- Protomon/ProtomonTransmit.cpp
+++ Protomon/ProtomonTransmit.cpp
@@ -13,7 +13,7 @@ ProtomonTransmit::ProtomonTransmit(uint8_t freq){
 }
 
 int ProtomonTransmit::transmit(){
-	//Do things here
+	//Do things here with PWM pin
 }
 
 void ProtomonTransmit::setFreq(uint8_t freq){
@@ -21,10 +21,28 @@ void ProtomonTransmit::setFreq(uint8_t freq){
 }
 
 void ProtomonTransmit::cypherBits(char downID[4]){
-	strcpy(data, "1");
-	strcat(data, downID);
-	//For loop: invert the downmonID
-	for(;;){
-		//make un-infinite
+	//Extern C code for string functions and looping
+	extern "C" {
+		int i = 0;
+		//Add start bit
+		strcpy(data, "1");
+		//Add downmon ID
+		strcat(data, downID);
+		//Add inverse of downmon ID:
+	
+	
+		//While loop: Check if char on position i is 1 or 0 and invert  
+		while(downID[i] != '\0'){
+			if(downID[i] = '1'){
+				i = 0;
+			} else {
+				i = 1;
+			}
+		}
+	
+		//Add parity bit
+		strcat(data,"1");
+		//Add stop bit
+		strcat(data,"1");
 	}
 }
