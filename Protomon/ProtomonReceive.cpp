8cd20c5414137f22064b184ac935c020448ac8d3
DESKTOP-ENRKL6H\brian <briankamperveen1@gmail.com>
2017-11-27 10:51:44 +0100

    Added stuff

--- Protomon/ProtomonReceive.cpp
+++ Protomon/ProtomonReceive.cpp
@@ -1,7 +1,13 @@
-// 
-// 
-// 
+#include "ProtomonTransmit.h"
 
-#include "ProtomonReceive.h"
+ProtomonReceive::ProtomonReceive(){
+	
+}
 
+int ProtomonReceive::receive(){
+	//Receive and return values
+}
 
+void ProtomonReceive::decypher(char data[12]){
+	//decypher incoming data
+}
