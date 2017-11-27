1003a6af192839c013b703e69f56b5f4ff4db541
TimM <timmeermans@live.nl>
2017-11-27 13:21:23 +0100

    cypherBits() should work now

--- Protomon/Protomon.ino
+++ Protomon/Protomon.ino
@@ -1,26 +1,5 @@
 #include "ProtomonReceive.h"
-#include "Protomon.h"
+#include "ProtomonTransmit.h"
 int main(){
-	DDRB |= (1<<DDB5);
-	DDRB &= ~(1<<DDB0);
-	//Initialize int i
-	int i = 0;
-	for(;;){
-		PORTB |= (1<<PORTB5);
-		_delay_ms(20);
-
-
-		PORTB &= ~(1<<PORTB5);
-		_delay_ms(10);
-
-		PORTB |= (1<<PORTB5);
-		_delay_ms(10);
-
-
-		PORTB &= ~(1<<PORTB5);
-		_delay_ms(20);
-
-
-		_delay_ms(100);
-	}
+	
 }
